c3d5d2e71cf8fc426ebe3948dafe985859f06aa9
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-10-21 18:52:39 +0200

    errore unchecked runtime.....

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 "use strict";
 
-// const btnCalculate = document.getElementById('btn-calculate');
+const btnCalculate = document.getElementById('btn-calculate');
 
-// btnCalculate.addEventListener('click', function(){
-//     const fullName = document.getElementById('full-name').value;
-//     const distance = Number(document.getElementById('km').value);
-//     const ageSelected = document.getElementById('age').selectedIndex;
-//     const age = ageSelected.options[ageSelected];
+btnCalculate.addEventListener('click', function(){
+    const fullName = document.getElementById('full-name').value;
+    const distance = Number(document.getElementById('km').value);
+    const ageSelected = document.getElementById('age').selectedIndex;
+    const age = ageSelected.options[ageSelected];
 
-//     if(!isNaN(distance) || km <= 0){
-//         console.log(distance, fullName, age);
-//     }else{
-//         alert("Nei chilometri DEVI inserire un numero superiore o uguale a 0, la pagina verrà ricaricata");
-//         window.location.reload;
-//     }
-// });
+    if(!isNaN(distance) || km <= 0){
+         console.log(distance, fullName, age);
+     }else{
+        alert("Nei chilometri DEVI inserire un numero superiore o uguale a 0, la pagina verrà ricaricata");
+         window.location.reload;
+     }
+});
