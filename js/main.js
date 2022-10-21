1d06c088af44139b40812c0b30e13a13d076e24f
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-10-21 17:30:51 +0200

    varie

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 "use strict";
-const btnCalculate = document.getElementById('btn-calculate');
 
-btnCalculate.addEventListener('click', function(){
-    const fullName = document.getElementById('full-name').value;
-    const distance = Number(document.getElementById('km').value);
-    const age = document.getElementById('age').options[document.pform.selectedIndex].value;
+// const btnCalculate = document.getElementById('btn-calculate');
 
-    if(!isNaN(distance) || km <= 0){
-        console.log(distance, fullName, age);
-    }else{
-        alert("Nei chilometri DEVI inserire un numero superiore o uguale a 0, la pagina verrà ricaricata");
-        window.location.reload;
-    }
-});
+// btnCalculate.addEventListener('click', function(){
+//     const fullName = document.getElementById('full-name').value;
+//     const distance = Number(document.getElementById('km').value);
+//     const ageSelected = document.getElementById('age').selectedIndex;
+//     const age = ageSelected.options[ageSelected];
+
+//     if(!isNaN(distance) || km <= 0){
+//         console.log(distance, fullName, age);
+//     }else{
+//         alert("Nei chilometri DEVI inserire un numero superiore o uguale a 0, la pagina verrà ricaricata");
+//         window.location.reload;
+//     }
+// });
